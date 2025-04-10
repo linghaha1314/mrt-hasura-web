8cc35ba774bb1fb571fbe3e072b78161f2acb94e
linghaha1314 <40912860+linghaha1314@users.noreply.github.com>
2025-04-10 15:59:20 +0800

    修改数据库配置22

--- pro_config.js
+++ pro_config.js
@@ -3,9 +3,9 @@ const config = {
     // poolObj: {
     //     user: 'postgres', host: '127.0.0.1', database: 'postgres', password: '1234', port: 5433
     // }
-    refUrl: "http://116.63.137.233:8080", //crud服务
+    refUrl: "http://172.25.255.54:8080", //crud服务
     poolObj: {
-        user: 'postgres', host: '116.63.137.233', database: 'postgres', password: 'kbds1234', port: 5432
+        user: 'postgres', host: '172.25.255.54', database: 'postgres', password: 'kbds1234', port: 5432
     }
 }
 module.exports = config;
